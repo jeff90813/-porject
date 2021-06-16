317a5bb239f879c93aa07e9b0da346a59f4196e8
ass77548921 <a.smart.andrew@gmail.com>
2021-06-16 15:00:06 +0800

    new

--- controllers/services/ramServ.js
+++ controllers/services/ramServ.js
@@ -1,8 +1,18 @@
+const { raw } = require('express');
 const Ram = require('../../models/ramModel')
 
 exports.fetchRam = async () => {
     try{
         const [rows] = await Ram.fetchAll();
+        return rows;
+    }catch(err){
+        return {message: 'Error on getting data'}
+    }
+}
+
+exports.fetchram_id = async(id) => {
+    try{
+        const [rows] = await Ram.fetchID(id);
         return rows;
     }catch(err){
         return {message: 'Error on getting data'}
--- routes/api.js
+++ routes/api.js
@@ -6,7 +6,8 @@ const apiController = require('../controllers/apiController');
 router.get('/', apiController.getcpu);
 router.get('/motherboard', apiController.getmotherboard);
 router.get('/ram', apiController.getram);
-router.get('/motherboard_id/:socket_id',apiController.getmotherboard_id);
+router.get('/motherboard_id/:id',apiController.getmotherboard_id); 
+router.get('/ram_id/:cpu_id/:id',apiController.getram_id);
 router.get('/list', apiController.getselection);
 
 module.exports = router;
