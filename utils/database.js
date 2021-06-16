b854d9216132ea5a47eb6ee40bf3a6dc63041d39
ass77548921 <76846676+ass77548921@users.noreply.github.com>
2021-06-16 16:43:24 +0800

    database (#7)
    * 408410669
    
    * first commit
    
    * 6/14
    
    * delete
    
    * test
    
    * 222
    
    * 408410669
    
    * CPU_CRUD
    
    * mo_id
    
    * new
    
    * select
    
    * select
    
    * Update database.js
    
    Co-authored-by: jeff90813 <40972402+jeff90813@users.noreply.github.com>

--- utils/database.js
+++ utils/database.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 
 // create database connection
 const pool = mysql.createPool({ 
-    host:'localhost', 
-    user: 'root', 
-    password: '0000',
-    database: 'db'
+   host:'us-cdbr-east-04.cleardb.com', 
+    user: 'b618d314204b45', 
+    password: '1b73004c',
+    database: 'heroku_87ea95d952d2406'
   })
 
 module.exports = pool.promise();
